8d1ed9c2dc8249f9a74614016a483ec2b0c5538f
vigneswaran20 <86698190+vigneswaran20@users.noreply.github.com>
2022-04-04 07:24:03 +0530

    Create nse_quick_analysis.r

---
+++ nse_quick_analysis.r
@@ -0,0 +1,17 @@
+
+
+devtools::install_github("rsquaredacademy/nse2r")
+library(nse2r)
+###-------------------------------------------------
+#Top Gainers
+nse_stock_top_gainers() 
+
+#52 week High
+nse_stock_year_high()
+
+#Most actively traded
+nse_stock_most_traded() 
+
+?nse2r
+
+nse_app()
